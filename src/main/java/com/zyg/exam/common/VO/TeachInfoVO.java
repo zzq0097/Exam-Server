103b4fa072b010e86f1b8a954f7554f98be753ce
ZZQ <18736271057@163.com>
2020-04-07 22:01:05 +0800

    v001

---
+++ src/main/java/com/zyg/exam/common/VO/TeachInfoVO.java
@@ -0,0 +1,18 @@
+package com.zyg.exam.common.VO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeachInfoVO {
+    private int id;
+    private int teacherid;
+    private String teachername;
+    private int classid;
+    private String classname;
+    private int courseid;
+    private String coursename;
+}
