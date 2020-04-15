bfc7c678af32fcec5329656b824252ba1c01442f
ZZQ <18736271057@163.com>
2020-04-15 16:16:45 +0800

    v001

--- src/main/java/com/zyg/exam/controller/TestController.java
+++ src/main/java/com/zyg/exam/controller/TestController.java
@@ -16,61 +16,61 @@ import java.util.List;
 @Slf4j
 @RestController
 public class TestController {
-
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private UserDao userDao;
-
-    @RequestMapping("/userCount")
-    public int count(){
-        return userDao.selectCount(null);
-    }
-
-    @RequestMapping("/login")
-    public JsonBean login(String username,String password){
-        return userService.isLogin(username,password);
-    }
-
-    @RequestMapping("/updateUser")
-    public JsonBean updateUser(User user){
-        return userService.updateUser(user);
-    }
-
-    @RequestMapping("/deleteUser")
-    public JsonBean deleteUser(int[] ids){
-        if (ids.length>0){
-            return userService.deleteUser(ids);
-        } else {
-            return new JsonBean(200,"未选择条目：删除了0条数据",null);
-        }
-    }
-
-    @RequestMapping("/insertUser")
-    public JsonBean insertUser(User user){
-        return userService.insertUser(user);
-    }
-
-    @RequestMapping("/selectUser")
-    public ResVO selectByRole(UserDTO userDTO){
-        return userService.selectUser(userDTO);
-    }
-
-    @RequestMapping("/getCourse")
-    public List<String> selectCourse(int id){
-        return userService.selectCourse(id);
-    }
-
-    @RequestMapping("/importUser")
-    public JsonBean addQuestion(@RequestParam("file") MultipartFile file){
-        JsonBean jsonBean=new JsonBean();
-        String fileName = file.getOriginalFilename();
-        log.info("{}",fileName);
-        try {
-            jsonBean=userService.batchImport(fileName,file);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return  jsonBean;
-    }
+//
+//    @Autowired
+//    private UserService userService;
+//    @Autowired
+//    private UserDao userDao;
+//
+//    @RequestMapping("/userCount")
+//    public int count(){
+//        return userDao.selectCount(null);
+//    }
+//
+//    @RequestMapping("/login")
+//    public JsonBean login(String username,String password){
+//        return userService.isLogin(username,password);
+//    }
+//
+//    @RequestMapping("/updateUser")
+//    public JsonBean updateUser(User user){
+//        return userService.updateUser(user);
+//    }
+//
+//    @RequestMapping("/deleteUser")
+//    public JsonBean deleteUser(int[] ids){
+//        if (ids.length>0){
+//            return userService.deleteUser(ids);
+//        } else {
+//            return new JsonBean(200,"未选择条目：删除了0条数据",null);
+//        }
+//    }
+//
+//    @RequestMapping("/insertUser")
+//    public JsonBean insertUser(User user){
+//        return userService.insertUser(user);
+//    }
+//
+//    @RequestMapping("/selectUser")
+//    public ResVO selectByRole(UserDTO userDTO){
+//        return userService.selectUser(userDTO);
+//    }
+//
+//    @RequestMapping("/getCourse")
+//    public List<String> selectCourse(int id){
+//        return userService.selectCourse(id);
+//    }
+//
+//    @RequestMapping("/importUser")
+//    public JsonBean addQuestion(@RequestParam("file") MultipartFile file){
+//        JsonBean jsonBean=new JsonBean();
+//        String fileName = file.getOriginalFilename();
+//        log.info("{}",fileName);
+//        try {
+//            jsonBean=userService.batchImport(fileName,file);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return  jsonBean;
+//    }
 }
---
+++ src/main/java/com/zyg/exam/controller/UserController.java
@@ -0,0 +1,105 @@
+package com.zyg.exam.controller;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.zyg.exam.common.DTO.AdminDTO;
+import com.zyg.exam.common.DTO.StudentDTO;
+import com.zyg.exam.common.DTO.TeacherDTO;
+import com.zyg.exam.common.JsonBean;
+import com.zyg.exam.common.ResVO;
+import com.zyg.exam.model.User;
+import com.zyg.exam.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author ZZQ
+ * @since 2020-04-15
+ */
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/login")
+    public JsonBean login(String username, String password){
+        Map<String, Object> map = new HashMap<>();
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        map.put("username",username);
+        map.put("password",password);
+        queryWrapper.allEq(map);
+        List<User> list = userService.list(queryWrapper);
+        if (list.size() == 1 && (list.get(0).getRole().equals("1") || list.get(0).getRole().equals("2"))){
+            list.get(0).setPassword("");
+            return new JsonBean(200,"登录成功",list.get(0));
+        } else {
+            return new JsonBean(500,"登录失败",null);
+        }
+    }
+
+    @RequestMapping("/selectStudent")
+    public ResVO selectStudent(StudentDTO studentDTO){
+//        Page<User> page = new Page<>(studentDTO.getPageIndex(),studentDTO.getPageSize());
+//        Map<String, Object> map = new HashMap<>();
+//        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+//        map.put("role",3);
+//        if (studentDTO.getClassid() != null){
+//            map.put("classid",studentDTO.getClassid());
+//        }
+//        queryWrapper.allEq(map);
+//        IPage<User> list = userService.page(page,queryWrapper);
+//        return new ResVO(list.getRecords(),list.getTotal());
+        return new ResVO();
+    }
+    @RequestMapping("/selectTeacher")
+    public ResVO selectTeacher(TeacherDTO teacherDTO){
+        Page<User> page = new Page<>(teacherDTO.getPageIndex(),teacherDTO.getPageSize());
+        Map<String, Object> map = new HashMap<>();
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        map.put("role",2);
+        if (StringUtils.isNotBlank(teacherDTO.getName())){
+            map.put("name",teacherDTO.getName());
+        }
+        queryWrapper.allEq(map);
+        IPage<User> list = userService.page(page,queryWrapper);
+        return new ResVO(list.getRecords(),list.getTotal());
+    }
+    @RequestMapping("/selectAdmin")
+    public ResVO selectAdmin(AdminDTO adminDTO){
+        Page<User> page = new Page<>(adminDTO.getPageIndex(),adminDTO.getPageSize());
+        Map<String, Object> map = new HashMap<>();
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        map.put("role",1);
+        queryWrapper.allEq(map);
+        IPage<User> list = userService.page(page,queryWrapper);
+        return new ResVO(list.getRecords(),list.getTotal());
+    }
+    // 公共接口
+    @RequestMapping("/updateUser")
+    public JsonBean updateUser(User user){
+        return new JsonBean(200,"success",userService.updateById(user));
+    }
+    @RequestMapping("/insertUser")
+    public JsonBean insertUser(User user){
+        return new JsonBean(200,"success",userService.save(user));
+    }
+    @RequestMapping("/deleteUser")
+    public JsonBean deleteUser(@RequestParam(value = "ids") List<Integer> ids){
+        return new JsonBean(200,"success",userService.removeByIds(ids));
+    }
+}
+
