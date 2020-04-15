46dbc2fda9e501df0feba95101bbaa635dca933a
ZZQ <18736271057@163.com>
2020-04-15 16:28:38 +0800

    courseVO 查询  teachVO 查询 StudentVO 查询

--- src/main/java/com/zyg/exam/common/VO/UserVO.java
+++ src/main/java/com/zyg/exam/common/VO/UserVO.java
@@ -1,46 +1,12 @@
 package com.zyg.exam.common.VO;
 
-import lombok.*;
+import lombok.Data;
 
-import java.util.Date;
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserVO {
-
     private Integer id;
-
-    /**
-     * 教师名称
-     */
     private String name;
-
-    /**
-     * 密码
-     */
     private String password;
-
-    /**
-     * 电话
-     */
     private String tel;
-
-    /**
-     * 工号
-     */
     private String username;
-
-    /**
-     * 角色
-     */
-    private String role;
-
-    /**
-     * 学生所属班级
-     */
-    private String classname;
-
-
-
-    private Date insert_date;
 }
