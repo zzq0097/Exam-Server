3b32ebe2e4db9f76c29d60272ae4e0d3935c8b8e
xiayingjie_ext <xiayingjie-pccw@139.com>
2020-04-16 20:23:05 +0800

    组卷以及事务回滚

--- src/main/java/com/zyg/exam/common/AllRandom.java
+++ src/main/java/com/zyg/exam/common/AllRandom.java
@@ -25,16 +25,18 @@ private PaperDao paperDao;
         System.out.println("listSize"+list.size());
         //把随机取得的数据存储在 listRandom 中
         List<Integer> listRandom = new ArrayList<Integer>();
-        //随机取出n条不重复的数据,这里我设置随机取3条数据
-        for (int i = n; i >=1; i--) {
-            Random random = new Random();
-            Math.random();
-            //在数组大小之间产生一个随机数 j
-            int j = random.nextInt(list.size()-1);
-            //取得list 中下标为j 的数据存储到 listRandom 中
-            listRandom.add(list.get(j));
-            //把已取到的数据移除,避免下次再次取到出现重复
-            list.remove(j);
+        if (list.size()-1>0) {
+            //随机取出n条不重复的数据,这里我设置随机取3条数据
+            for (int i = n; i >= 1; i--) {
+                Random random = new Random();
+                Math.random();
+                //在数组大小之间产生一个随机数 j
+                int j = random.nextInt(list.size() - 1);
+                //取得list 中下标为j 的数据存储到 listRandom 中
+                listRandom.add(list.get(j));
+                //把已取到的数据移除,避免下次再次取到出现重复
+                list.remove(j);
+            }
         }
         return listRandom;
     }
