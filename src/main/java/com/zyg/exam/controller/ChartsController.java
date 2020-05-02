9e08b7ef1103b8fc9a17025da3681ebe39c08570
ZZQ <18736271057@163.com>
2020-05-02 15:30:44 +0800

    v001

--- src/main/java/com/zyg/exam/controller/ChartsController.java
+++ src/main/java/com/zyg/exam/controller/ChartsController.java
@@ -1,5 +1,10 @@
 package com.zyg.exam.controller;
 
+import com.zyg.exam.common.VO.BarVO;
+import com.zyg.exam.common.VO.SpreadVO;
+import com.zyg.exam.model.Class;
+import com.zyg.exam.service.RecordService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +12,9 @@ import java.util.List;
 
 @RestController
 public class ChartsController {
+
+    @Autowired
+    private RecordService recordService;
 
     // 柱状图
     @RequestMapping("/barChart")
@@ -25,6 +33,21 @@ public class ChartsController {
     public List<?> lineChart(Integer id){
 
         return null;
+    }
+
+    @RequestMapping("/selectAverage")
+    public List<BarVO> selectAverage(Integer paperid){
+        return recordService.selectAverage(paperid);
+    }
+
+    @RequestMapping("/selectSpread")
+    public List<SpreadVO> selectSpread(Integer paperid, Integer classid){
+        return recordService.selectSpread(paperid,classid);
+    }
+
+    @RequestMapping("/selectClassByPaperid")
+    public List<Class> selectClass(Integer paperid){
+        return recordService.selectClass(paperid);
     }
 
 }
--- src/main/java/com/zyg/exam/controller/RecordController.java
+++ src/main/java/com/zyg/exam/controller/RecordController.java
@@ -1,20 +1,13 @@
 package com.zyg.exam.controller;
 
 import com.zyg.exam.common.DTO.CorrectPaperDTO;
-import com.zyg.exam.common.DTO.PaperQuestionDTO;
 import com.zyg.exam.common.DTO.RecordDTO;
 import com.zyg.exam.common.JsonBean;
 import com.zyg.exam.common.ResVO;
-import com.zyg.exam.common.VO.BarVO;
-import com.zyg.exam.common.VO.SpreadVO;
 import com.zyg.exam.dao.RecordDao;
-import com.zyg.exam.model.Class;
 import com.zyg.exam.service.RecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 public class RecordController {
@@ -43,25 +36,8 @@ public class RecordController {
         System.out.println(correctPaperDTO);
         return recordService.correctPaper(correctPaperDTO);
     }
-
     @RequestMapping("/selectQuestionByRecord")
     public JsonBean selectQuestionByRecord(Integer recordid){
-
         return recordService.selectQuestionByRecord(recordid);
-    }
-
-    @RequestMapping("/selectAverage")
-    public List<BarVO> selectAverage(Integer paperid){
-        return recordService.selectAverage(paperid);
-    }
-
-    @RequestMapping("/selectSpread")
-    public List<SpreadVO> selectSpread(Integer paperid,Integer classid){
-        return recordService.selectSpread(paperid,classid);
-    }
-
-    @RequestMapping("/selectClassByPaperid")
-    public List<Class> selectClass(Integer paperid){
-        return recordService.selectClass(paperid);
     }
 }
