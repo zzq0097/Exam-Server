8d1645bfe93f9b02fc4f59870869c16cee5069a0
ZZQ <18736271057@163.com>
2020-05-05 09:58:20 +0800

    v001

---
+++ src/main/java/com/zyg/exam/common/VO/LineVO.java
@@ -0,0 +1,11 @@
+package com.zyg.exam.common.VO;
+
+import lombok.Data;
+
+@Data
+public class LineVO {
+    private String time;
+    private int maxScore;
+    private int avgScore;
+    private int minScore;
+}
